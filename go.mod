828675ded299e122205713ad392a39411fadd929
Cameron <cammbilly8@gmail.com>
2024-02-11 23:03:59 -0500

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cmbilly8/goroku
+
+go 1.21.6
---
+++ goroku.go
@@ -0,0 +1,45 @@
+package goroku
+
+import (
+	"bytes"
+	"fmt"
+	"net"
+	"net/http"
+)
+
+type Client struct {
+	url string
+}
+
+const (
+	cmdTogglePower = "keypress/power"
+	cmdVolumeUp    = "keypress/VolumeUp"
+)
+
+func NewClientByIP(device_ip net.IP) *Client {
+	url := fmt.Sprintf("http://%v:8060", device_ip.String())
+	return &Client{
+		url: url,
+	}
+}
+
+func (c *Client) TogglePower() error {
+	err := sendCommand(cmdTogglePower, c.url)
+	return err
+}
+
+func (c *Client) VolumeUp() error {
+	err := sendCommand(cmdVolumeUp, c.url)
+	return err
+}
+
+func getEmptyReader() *bytes.Reader {
+	jsonBody := []byte("")
+	return bytes.NewReader(jsonBody)
+}
+
+func sendCommand(cmd string, url string) error {
+	requestURL := fmt.Sprintf("%v/%v", url, cmd)
+	_, err := http.NewRequest(http.MethodPost, requestURL, getEmptyReader())
+	return err
+}
