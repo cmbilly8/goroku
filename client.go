24b63aa5e4f801dd00ece17c6b2f7194a6520937
Cameron <cammbilly8@gmail.com>
2024-04-27 21:37:57 -0400

    :^)

---
+++ client.go
@@ -0,0 +1,119 @@
+package goroku
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+)
+
+const (
+	CmdTogglePower   = "keypress/power"
+	CmdVolumeUp      = "keypress/VolumeUp"
+	CmdVolumeDown    = "keypress/VolumeDown"
+	CmdVolumeMute    = "keypress/VolumeMute"
+	CmdHome          = "keypress/Home"
+	CmdReverse       = "keypress/Rev"
+	CmdForward       = "keypress/Fwd"
+	CmdPlay          = "keypress/Play"
+	CmdSelect        = "keypress/Select"
+	CmdLeft          = "keypress/Left"
+	CmdRight         = "keypress/Right"
+	CmdDown          = "keypress/Down"
+	CmdUp            = "keypress/Up"
+	CmdBack          = "keypress/Back"
+	CmdSkipBack      = "keypress/InstantReplay"
+	CmdInfo          = "keypress/Info"
+	CmdBackspace     = "keypress/Backspace"
+	CmdSearch        = "keypress/Search"
+	CmdEnter         = "keypress/Enter"
+	queryDeviceInfo  = "query/device-info"
+	queryMediaPlayer = "query/media-player"
+)
+
+type Client struct {
+	url        string
+	httpClient *http.Client
+	reader     *bytes.Reader
+}
+
+func NewClientByIP(device_ip net.IP) *Client {
+	url := fmt.Sprintf("http://%v:8060", device_ip.String())
+	reader := makeEmptyReader()
+	return &Client{
+		url:        url,
+		httpClient: &http.Client{},
+		reader:     reader,
+	}
+}
+
+func (c *Client) Url() string {
+	return c.url
+}
+
+func makeEmptyReader() *bytes.Reader {
+	jsonBody := []byte("")
+	return bytes.NewReader(jsonBody)
+}
+
+func (c *Client) SendCommand(Cmd string) error {
+	requestURL := fmt.Sprintf("%v/%v", c.url, Cmd)
+	fmt.Printf("Sending command: %v", requestURL)
+	req, _ := http.NewRequest(http.MethodPost, requestURL, c.reader)
+	_, err := c.httpClient.Do(req)
+	return err
+}
+
+func (c *Client) DeviceInfo() (*DeviceInfo, error) {
+	var di DeviceInfo
+	err := c.populateStructFromGetRequest(queryDeviceInfo, &di)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil, err
+	}
+	return &di, nil
+}
+
+func (c *Client) MediaPlayer() (*MediaPlayer, error) {
+	var mp MediaPlayer
+	err := c.populateStructFromGetRequest(queryMediaPlayer, &mp)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil, err
+	}
+	return &mp, nil
+}
+
+func (c *Client) populateStructFromGetRequest(endpoint string, v interface{}) error {
+	requestURL := fmt.Sprintf("%v/%v", c.url, endpoint)
+	fmt.Printf("Sending command: %v\n", requestURL)
+
+	request, err := http.NewRequest(http.MethodGet, requestURL, c.reader)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return err
+	}
+
+	response, err := c.httpClient.Do(request)
+	if err != nil {
+		fmt.Println("Problem sending request:", err)
+		return err
+	}
+	defer response.Body.Close()
+
+	responseBody, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Could not read the response body:", err)
+		return err
+	}
+
+	err = xml.Unmarshal(responseBody, v)
+	if err != nil {
+		fmt.Println("Problem unmarshalling response:", err)
+		return err
+	}
+
+	return nil
+}
--- goroku_test.go
+++ goroku_test.go
@@ -10,59 +10,45 @@ import (
 	"github.com/cmbilly8/goroku"
 )
 
-// func TestClient_VolumeUp(t *testing.T) {
+// func TestClient_PowerOn(t *testing.T) {
 // 	ip := net.IPv4(192, 168, 40, 100)
 // 	client := goroku.NewClientByIP(ip)
-// 	err := client.SendCommand(goroku.CmdVolumeUp)
+// 	err := client.SendCommand(goroku.CmdTogglePower)
 // 	if err != nil {
 // 		t.Log(err)
 // 		t.Fail()
 // 	}
 // }
 
-func TestClient_GetDeviceInfo(t *testing.T) {
+func TestClient_DeviceInfo(t *testing.T) {
 	fmt.Println("Start get device info test")
 	ip := net.IPv4(192, 168, 40, 100)
 	client := goroku.NewClientByIP(ip)
-	deviceInfo, err := client.GetDeviceInfo()
+	deviceInfo, err := client.DeviceInfo()
 	if err != nil {
 		fmt.Print("error getting device info")
 		log.Fatal()
 	}
-	printStructFieldsDeviceInfo(deviceInfo)
+	printStructFields(deviceInfo)
 	fmt.Println("End get device info test\n\t\n\t")
 }
 
-func printStructFieldsDeviceInfo(di *goroku.DeviceInfo) {
-	fmt.Print("Response:\n")
-	val := reflect.ValueOf(di).Elem()
-	typ := val.Type()
-
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-		fieldName := typ.Field(i).Name
-		fieldValue := field.Interface()
-
-		fmt.Printf("%s: %v\n", fieldName, fieldValue)
-	}
-}
-
-func TestClient_GetMediaPlayer(t *testing.T) {
+func TestClient_MediaPlayer(t *testing.T) {
 	fmt.Println("Start get media player test")
 	ip := net.IPv4(192, 168, 40, 100)
 	client := goroku.NewClientByIP(ip)
-	mediaPlayer, err := client.GetMediaPlayer()
+	mediaPlayer, err := client.MediaPlayer()
 	if err != nil {
 		fmt.Print("error getting device info")
 		log.Fatal()
 	}
-	printStructFieldsMediaPlayer(mediaPlayer)
+	printStructFields(mediaPlayer)
 	fmt.Println("End get media player test\n\t\n\t")
 }
 
-func printStructFieldsMediaPlayer(di *goroku.MediaPlayer) {
+func printStructFields(v interface{}) {
 	fmt.Print("Response:\n")
-	val := reflect.ValueOf(di).Elem()
+	val := reflect.ValueOf(v).Elem()
 	typ := val.Type()
 
 	for i := 0; i < val.NumField(); i++ {
